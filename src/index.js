6fa518a586713865549edcfecabc90b8103e513d
Oleksandr Bakai <saniabakay@gmail.com>
2025-07-07 20:44:37 +0200

    no bonus

--- src/index.js
+++ src/index.js
@@ -1,32 +1,52 @@
 const DURATION = 10; // 10 seconds
-let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
-
+let remainingTime = document.getElementById('time');
+console.log(remainingTime); // Countdown starting from 10
+let timer = 1000; // Variable to store the interval
 
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
+let startButton = document.getElementById('start-btn');
+startButton.addEventListener('click',startCountdown);
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
+  let i = DURATION;
+  remainingTime.textContent = i;
+  startButton.disabled = true;//Change style of the button to disabled
 
+  let intervalId = setInterval(function () {
+  if (i >= 0) {
+    console.log(i);
+    remainingTime.textContent = i;
 
-  // Your code goes here ...
+  } 
+  else {
+    console.log('End!');
+    clearInterval(intervalId);
+    remainingTime.textContent = 10;
+    startButton.disabled = false;//Remove button style to normal
+  }
+  i--;
+}, timer);
+
 }
-
-
+  
 
 
 // ITERATION 3: Show Toast
-function showToast(message) {
-  console.log("showToast called!");
 
-  // Your code goes here ...
+
+let toastCard = document.getElementById('toast');//find toast
+toastCard.classList.add('show');//make toast visible
+
+setTimeout(function(){
+  toastCard.classList.remove('show')
+},3000);
+
+
+
+// let timeOut = setTimeout(showToast, 1000);
 
 
 
@@ -35,4 +55,4 @@ function showToast(message) {
 
   // Your code goes here ...
 
-}
+
